67d046e31d5081fe9ca2719a133142471c25e91a
Steven LeBlanc <skleblan@gmail.com>
2017-10-28 11:36:36 -0500

    Unknown changes
    dont have time to figure it out now
    also, want to have time to practice more with branches

--- projects/judo_scoring.pl
+++ projects/judo_scoring.pl
@@ -2,20 +2,20 @@
 
 use Tk;
 
-my $mw = MainWindow;
+my $mw = new MainWindow;
 $mw -> geometry("800x600");
 $mw -> title("Judo Score Board");
 
-my $top_bar = $mw -> Frame(-height=>'100', -relief=>"solid");
-my $bottom_area = $mw -> Frame(-relief=>"solid");
+my $top_bar = $mw -> Frame(-height=>'100', -relief=>"solid", -background=>"red");
+my $bottom_area = $mw -> Frame(-relief=>"solid", -background=>"green");
 
-my $left_blue_area = $bottom_area -> Frame(-relief=>"solid", -background=>"blue");
-my $right_white_area = $bottom_area -> Frame(-relief=>"solid");
+my $left_blue_area = $bottom_area -> Frame(-relief=>"solid", -background=>"blue", -width=>300);
+my $right_white_area = $bottom_area -> Frame(-relief=>"solid", -background=>"white", -width=>200);
 
 $top_bar -> pack(-side=>"top", -fill=>"x");
 $bottom_area -> pack(-side=>"top", -fill=>"both");
 
-$left_blue_area -> grid(-row=>1, -column=>1, -sticky=>"nse");
-$right_white_area -> grid(-row=>1, -column=>2, -sticky=>"nsw");
+$left_blue_area -> pack(-side=>"left", -fill=>"y");
+$right_white_area -> pack(-side=>"left", -fill=>"y");
 
 MainLoop;
