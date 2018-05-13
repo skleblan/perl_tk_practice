15cb733c691c4adff7f97cc28d80ec6e892f1413
Steven LeBlanc <skleblan@gmail.com>
2018-05-13 12:16:48 -0500

    Added scoring components to judo_scoring.pl Added display labels and text labels for ippons, wazaaris, and shidos. Added variables to hold the number of ippons and wazaaris.
    Created new fonts that have different sizes.  Fonts cannot be used across
    different main windows.
    
    Removed sizing options from the blue and white frames.  Just turned on
    expand in both of them and set them to resize in both the x and y directions.
    After adding components inside of them, they automatically formatted themselves
    so that they each only took 50% of the width.
    
    Experimented with different relief/border styles and sizes.

--- projects/judo_scoring.pl
+++ projects/judo_scoring.pl
@@ -1,21 +1,72 @@
 #!/usr/bin/perl
 
 use Tk;
+use Tk::Font;
+
+my $blue_ippon_var = 0;
+my $blue_wazaari_var = 0;
+my $white_ippon_var = 0;
+my $white_wazaari_var = 0;
 
 my $mw = new MainWindow;
 $mw -> geometry("800x600");
 $mw -> title("Judo Score Board");
 
-my $top_bar = $mw -> Frame(-height=>'100', -relief=>"solid", -background=>"red");
-my $bottom_area = $mw -> Frame(-relief=>"solid", -background=>"green");
+##############
+# FONTS
+##############
+my $other_font = $mw -> Font(-size=>20);
+my $ippon_font = $mw -> Font(-size=>80);
+my $label_font = $mw -> Font(-size=>30);
 
-my $left_blue_area = $bottom_area -> Frame(-relief=>"solid", -background=>"blue", -width=>300);
-my $right_white_area = $bottom_area -> Frame(-relief=>"solid", -background=>"white", -width=>200);
+my $top_bar = $mw -> Frame(-height=>'100',
+                           -relief=>"solid",
+                           #solid: plain line
+                           #flat: none, default
+                           #groove/ridge are combos of raised and sunken
+                           -borderwidth=>3,
+                           -background=>"red");
+my $bottom_area = $mw -> Frame();
+
+my $left_blue_area = $bottom_area -> Frame(-background=>"blue", -relief=>"solid", -borderwidth=>3);
+my $right_white_area = $bottom_area -> Frame(-background=>"white", -relief=>"solid", -borderwidth=>3);
+
+my $blue_lbl = $left_blue_area -> Label(-text=>"Blue", -font=>$label_font, -relief=>"solid", -borderwidth=>1);
+my $blue_ippon_lbl = $left_blue_area -> Label(-text=>"Ippon: ");
+my $blue_ippon = $left_blue_area -> Label(-textvariable=>\$blue_ippon_var, -font=>$ippon_font);
+my $blue_waazari_lbl = $left_blue_area -> Label(-text=>"Wazaari: ");
+my $blue_waazari = $left_blue_area -> Label(-textvariable=>\$blue_wazaari_var, -font=>$other_font);
+my $blue_shidos_lbl = $left_blue_area -> Label(-text=>"Shidos: ");
+my $blue_shidos = $left_blue_area -> Label(-text=>"0", -font=>$other_font);
+
+
+my $white_lbl = $right_white_area -> Label(-text=>"White", -font=>$label_font, -relief=>"solid", -borderwidth=>1);
+my $white_ippon_lbl = $right_white_area -> Label(-text=>"Ippon: ");
+my $white_ippon = $right_white_area -> Label(-textvariable=>\$white_ippon_var, -font=>$ippon_font);
+my $white_waazari_lbl = $right_white_area -> Label(-text=>"Wazaari: ");
+my $white_waazari = $right_white_area -> Label(-textvariable=>\$white_wazaari_var, -font=>$other_font);
+my $white_shidos_lbl = $right_white_area -> Label(-text=>"Shidos: ");
+my $white_shidos = $right_white_area -> Label(-text=>"0", -font=>$other_font);
+
+############################################
+# ARRANGE GEOMETRY
+############################################
 
 $top_bar -> pack(-side=>"top", -fill=>"x");
-$bottom_area -> pack(-side=>"top", -fill=>"both");
+$bottom_area -> pack(-side=>"top", -fill=>"both", -expand=>1);
 
-$left_blue_area -> pack(-side=>"left", -fill=>"y");
-$right_white_area -> pack(-side=>"left", -fill=>"y");
+$left_blue_area -> pack(-side=>"left", -expand=>1, -fill=>"both");
+#note: when i actually put something inside the controls, it seemed like
+#  they went to 50% and 50% on their own
+
+$blue_lbl -> grid(-columnspan=>4);
+$blue_ippon_lbl -> grid($blue_ippon, $blue_waazari_lbl, $blue_waazari, -sticky=>"nsew");
+$blue_shidos_lbl -> grid($blue_shidos, -sticky=>"nsew");
+
+$right_white_area -> pack(-side=>"right", -expand=>1, -fill=>"both");
+
+$white_lbl -> grid(-columnspan=>4, -sticky=>"nsew");
+$white_ippon_lbl -> grid($white_ippon, $white_waazari_lbl, $white_waazari, -sticky=>"nsew");
+$white_shidos_lbl -> grid($white_shidos, -sticky=>"nsew", -columnspan=>2);
 
 MainLoop;
